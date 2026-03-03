a7846b9e25248c10bea57cb7d58a73be5c813652
GabriLPDA22 <gsaiz.bajo@gmail.com>
2026-03-03 14:09:00 +0100

    Integrate sitemap functionality and update site configuration
    - Added @astrojs/sitemap integration to generate a sitemap excluding 404 and 500 pages.
    - Updated site URL in configuration to "https://divinidad000.com".
    - Modified robots.txt to reflect the new sitemap URL and disallow 404 and 500 pages.
    - Enhanced BaseLayout and various pages with structured data for better SEO and user experience.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://divinidad000.com",
   output: "static",
   integrations: [
     tailwind({
       applyBaseStyles: false,
+    }),
+    sitemap({
+      filter: (page) =>
+        !page.includes("/404") && !page.includes("/500"),
     }),
   ],
 });
